2f8d43faadb4106e3f2abc7b82a805dada408e61
EC2 Default User <ec2-user@ip-172-31-87-30.ec2.internal>
2021-04-30 03:38:26 +0000

    Fix wrong file name.

--- test/postImage.sh
+++ test/postImage.sh
@@ -1,3 +1,2 @@
 baseUrl=acf56c5c79f82ce12.awsglobalaccelerator.com
-curl -i -X POST -H "Content-Type: multipart/form-data" -F "image=@DeepRacer.jpg" "http://$baseUrl?ProjectName=AWSDeepRacer&VersionName=first"
-# curl -X POST --data-binary @DeepRacer.jpg http://Globa-Image-1Y2KX2UBPHOL6-617453797.us-east-1.elb.amazonaws.com/DeeprRacer/first
+curl -i -X POST -H "Content-Type: multipart/form-data" -F "image=@DeepRacer.jpg" "http://$baseUrl?ProjectName=AWSDeepRacer&VersionName=third"
